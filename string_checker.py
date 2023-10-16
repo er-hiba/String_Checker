1f05f28ec3806491935faed3f61008bf4307fe1b
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-10-16 23:21:54 +0100

    added the files

---
+++ string_checker.py
@@ -0,0 +1,9 @@
+# Input: Ask the user to enter a string
+string = input("Enter a string: ")
+
+# Check if the entered string is empty
+if string == "" :
+    print("This string is empty.")
+else:
+    print("This string isn't empty.")
+    
